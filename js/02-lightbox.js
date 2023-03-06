0481a1be70d9e4a62039590922660fcdf3a35180
Karinkarllina <karinkhaliullina@gmail.com>
2023-03-06 13:18:06 +0200

    ДЗ-7
    Завдання №2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(item) {
+
+    return item.map(({ preview, original, description }) => {
+        return `<li>
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  </li>`
+}).join('')
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: '250'
+});
